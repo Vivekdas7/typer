fa459fb26a84148a4c5884564102384427cd3115
akash1047 <94453657+akash1047@users.noreply.github.com>
2023-01-22 11:34:48 +0530

    adding required dependencies

---
+++ src/about.rs
@@ -0,0 +1,17 @@
+pub mod github {
+    #[derive(Debug, PartialEq)]
+    pub struct Profile<'a>(&'a str);
+
+    impl Profile<'_> {
+        pub fn github_link(&self) -> String {
+            format!("https://github.com/{}", self.0)
+        }
+    }
+
+    pub const GITHUB_ICON: char = '';
+    pub const GITHUB_REPO_LINK: &str = "https://github.com/akash1047/typer";
+    pub const CONTRIBUTERS: [Profile; 1] = [
+        // Profile("<github-id>")
+        Profile("akash1047"),
+    ];
+}
